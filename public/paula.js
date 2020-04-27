164fed581795dfa0d4dd1ca207149d536c9f37fa
Paulahu <paulahu1995@gmail.com>
2020-04-27 16:09:15 +0800

    用JSONP实现跨域

--- public/paula.js
+++ public/paula.js
@@ -1,8 +1,30 @@
-const request = new XMLHttpRequest()
-request.open('get', 'http://qq.com:8888/friends.json')
-request.onreadystatechange = () => {
-    if (request.readyState === 4 && request.status === 200) {
-        console.log(request.response)
-    }
+function jsonp() {
+    return new Promise((resolve, reject) => {
+        const random = 'paulaJSONPCallbackName' + Math.random()
+        window[random] = () => {
+            resolve(data) //如果成功执行resolve'
+        }
+
+        const script = document.createElement('script')
+        script.src = `http://qq.com:8888/friends.js?callback=${random}`
+
+        //如果不使用函数就不用onload
+        // script.onload = () => {
+        //     console.log(window.xxx)
+        // }
+
+        //删除执行完的script标签
+        script.onload = () => {
+            script.remove()
+        }
+        script.onerror = () => {
+            reject() //如果失败执行reject
+        }
+        document.body.appendChild(script)
+    })
 }
-request.send()
+
+jsonp('http://qq.com:8888/friends.js')
+    .then((data) => {
+        console.log(data)
+    })
